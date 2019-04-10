a3d5743bd4f066efa5fbafa164d838e8dec22379
jonaititas <44038161+jonaititas@users.noreply.github.com>
2019-04-10 17:04:23 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1D5LaGppFh_FFp8ZqdIFaIpZ_CEE6XU95'
-export_file_name = 'stage-2.pth'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1sSBhovO32WmqyFMpMvYOIAgZkKT-xoLo'
+export_file_name = 'export.pkl'
 
-classes = ['skvernelis', 'kubilius']
+classes = ['skvernelis', 'kubilius', 'paksas']
 path = Path(__file__).parent
 
 app = Starlette()
